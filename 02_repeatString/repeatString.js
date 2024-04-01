35ed7706f3d802e90545692dba208a37c6b8a565
lwazi-nxumalo <lwazinx@outlook.com>
2024-04-01 12:24:01 +0200

    3 tests done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
+const repeatString = function( word , times) {
+   if (times > 3) return "ERROR";
+   let string = "hey"
+ for (let i = 1; i < times ; i++){
+   string += word;
+ }
+ return string;
 
-};
-
+}
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let splitString = str.split(""); 
+    let reverseArray = splitString.reverse();
+    let joinArray = reverseArray.join("");
+    return joinArray;
 
 };
 
+reverseString("hello")
 // Do not edit below this line
 module.exports = reverseString;
